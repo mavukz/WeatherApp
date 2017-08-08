1cf5b74d89b5ee4dd554d8cc9a7331bb63313271
luntu <luntu@cowboyaliens.com>
2017-08-08 19:28:38 +0200

    ADDED: Visibility property

--- iOS/Views/MainViewController.cs
+++ iOS/Views/MainViewController.cs
@@ -3,29 +3,43 @@ using MvvmCross.Binding.BindingContext;
 
 namespace WeatherApp_v2.iOS
 {
-	public partial class MainViewController : MvxViewController<MainViewModel>
-	{
-		
-		public MainViewController() : base("MainViewController", null)
+    public partial class MainViewController : MvxViewController<MainViewModel>
+    {
+
+        public MainViewController() : base("MainViewController", null)
         {
-		}
+        }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			NavigationController.NavigationBarHidden = true;
+        public bool Visibility
+        {
+            get 
+            {
+                return ActivityIndicator.Hidden;
+            }
+            set
+            {
+                ActivityIndicator.Hidden = !value;
+                ActivityIndicator.StartAnimating();
+            }
+        }
 
-			var bindingSet = this.CreateBindingSet<MainViewController, MainViewModel>();
-			bindingSet.Bind(CityTextField).To(vm => vm.City);
-			bindingSet.Bind(GetWeatherButton).To(vm => vm.GetWeatherAsync);
-			bindingSet.Apply();
-		}
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            NavigationController.NavigationBarHidden = true;
 
-		public override void ViewDidLayoutSubviews()
-		{
-			base.ViewDidLayoutSubviews();
-			GetWeatherButton.Layer.CornerRadius = 5f;
-		}
-	}
+            var bindingSet = this.CreateBindingSet<MainViewController, MainViewModel>();
+            bindingSet.Bind(CityTextField).To(ViewModel => ViewModel.City);
+            bindingSet.Bind(GetWeatherButton).To(ViewModel => ViewModel.GetWeatherAsync);
+            bindingSet.Bind(this).For(view => view.Visibility).To(ViewModel => ViewModel.Busy);
+            bindingSet.Apply();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            GetWeatherButton.Layer.CornerRadius = 5f;
+        }
+    }
 }
 
